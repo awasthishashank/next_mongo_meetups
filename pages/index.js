c7e16226d379d435e57a43ff4e3e2cfffbdd483b
awasthishashank <awasthishashank@gmail.comgit config --global user.username awasthishashank git config --global user.email awasthishashank@gmail.com>
2024-06-26 19:25:56 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,36 @@ import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList'
 // import { useState, useEffect } from "react";
 
-
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first meetup",
+    image: "https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg",
+    address: "148, Awas Vikas",
+    description: "A first meet up",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image: "https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg",
+    address: "148, Civil Lines",
+    description: "A second meet up",
+  },
+  {
+    id: "m3",
+    title: "A third  meetup",
+    image: "https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg",
+    address: "bypass ,Lucknow",
+    description: "A third meetup",
+  },
+  {
+    id: "m4",
+    title: "A fourth meetup",
+    image: "https://addicted2success.com/wp-content/uploads/2018/06/8-Reasons-You-Should-Join-a-Meetup-Group-Today.jpg",
+    address: "bypass ,Lucknow",
+    description: "A fourth meetup",
+  },
+];
 
 function Home(props) {
   // const [loadedMeetups, setLoadedMeetups] = useState([]);
@@ -29,7 +58,7 @@ export async function getStaticProps(){
       }))
     },
 
-    revalidate : 1
+    revalidate : 5
   }
 }
 export default Home;
